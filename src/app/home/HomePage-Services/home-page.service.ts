894c291ca91e0c2e6cd871195773bff0285cd5d6
Anil70135 <mandaanil452@gmail.com>
2025-05-12 11:38:10 +0530

    Update home-page.service.ts

--- src/app/home/HomePage-Services/home-page.service.ts
+++ src/app/home/HomePage-Services/home-page.service.ts
@@ -20,7 +20,7 @@ export class HomePageService {
 
   //Getting the Products from backend API
   getProducts():Observable<IProduct[]>{
-    let tempVar = this.http.get<IProduct[]>('https://quickkart-webservice20250509041247-fwc9c2f8a5btawax.canadacentral-01.azurewebsites.net/api/home/getproducts')
+    let tempVar = this.http.get<IProduct[]>('https://grpbapi.azure-api.net/api/home/getproducts')
     console.log(tempVar)
     return tempVar
   }
